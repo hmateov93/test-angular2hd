0e5436f19e278f6dfc418b56363c84fc3b73ed75
dalcay <dalcaydesopra@gmail.com>
2016-04-01 09:44:49 +0200

    Delete main.ts

--- app/main.ts
+++
@@ -1,7 +0,0 @@
-import 'rxjs/Rx';
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
-//import {UsersService} from './login-form/users.service';
-
-bootstrap(AppComponent);
-
